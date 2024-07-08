64f3c26ccf82f4b029df9d0585a67a47dfdc3b72
AntoCant <antonio.canteros.alberto@gmail.com>
2024-07-08 13:03:17 -0300

    api-payments

---
+++ src/main/java/com/demo/api_payments/ApiPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.api_payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/api_payments/ApiPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.api_payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
