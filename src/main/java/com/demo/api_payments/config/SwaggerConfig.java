3acc36d0fc337348f3e9db493acdbe49a6a81d52
Jhosep Adbel Romero Loa <jhromero.abx@gmail.com>
2024-08-25 21:32:59 -0500

    files to deploy

---
+++ src/main/java/com/demo/api_payments/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.demo.api_payments.config;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(Arrays.asList(
+                        new Server().url("http://localhost:8080").description("API Payments Dev Server"),
+                        new Server().url("https://api-payments-dev.abexa.pe").description("API Payments Deployed Server")
+                ))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .name("bearerAuth")
+                                .type(Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
